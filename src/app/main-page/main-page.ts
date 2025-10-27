96cc3d4433457f1cade43c410517b898dc83a2b9
Kevin Hase <hasekevin21@googlemail.com>
2025-10-27 09:42:10 +0100

    add more content

--- src/app/main-page/main-page.ts
+++ src/app/main-page/main-page.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+
+import { AboutMe } from '../components/about-me/about-me';
+import { SkillSet } from '../components/skill-set/skill-set';
+import { Projects } from '../components/projects/projects';
+import { Comments } from '../components/comments/comments';
+import { Contact } from '../components/contact/contact';
+import { LandingPage } from '../components/landing-page/landing-page';
 
 @Component({
   selector: 'app-main-page',
-  imports: [RouterLink],
+  imports: [LandingPage, AboutMe, SkillSet, Projects, Comments, Contact],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss'
 })
