d85efbe68c06e9a39a7b044aff3b018f595f00e2
Kevin Hase <hasekevin21@googlemail.com>
2025-10-27 17:45:36 +0100

    add icons and skill section

--- src/app/components/skill-set/skill-set.ts
+++ src/app/components/skill-set/skill-set.ts
@@ -8,5 +8,37 @@ import { RouterLink } from "@angular/router";
   styleUrl: './skill-set.scss'
 })
 export class SkillSet {
-
+  iconList = [{
+    name: 'Angular',
+    icon: './assets/icons/Skills/angular_icon.svg'
+  },
+  {
+    name: 'REST-API',
+    icon: './assets/icons/Skills/api_icon.svg'
+  },
+  {
+    name: 'CSS',
+    icon: './assets/icons/Skills/css_icon.svg'
+  },
+  {
+    name: 'HTML',
+    icon: './assets/icons/Skills/html_icon.svg'
+  },
+  {
+    name: 'JavaScript',
+    icon: './assets/icons/Skills/javascript_icon.svg'
+  },
+  {
+    name: 'TypeScript',
+    icon: './assets/icons/Skills/typescript_icon.svg'
+  },
+  {
+    name: 'Git',
+    icon: './assets/icons/Skills/git_icon.svg'
+  },
+  {
+    name: 'Growth Mindset',
+    icon: './assets/icons/Skills/mindset_icon.svg'
+  }
+];
 }
