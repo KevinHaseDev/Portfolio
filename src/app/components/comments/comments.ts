a9bc9296470d2597fab84bb9b7184e470e01a27d
Kevin Hase <hasekevin21@googlemail.com>
2025-10-28 20:49:49 +0100

    add comment section

--- src/app/components/comments/comments.ts
+++ src/app/components/comments/comments.ts
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './comments.scss'
 })
 export class Comments {
-
+comments: { comment: string, name: string }[] = [{
+  comment: "Code is clean and well structured. Always open for new technologies and willing to learn. A great team player!",
+  name: "John Doe"
+},
+{
+  comment: "Always willing to help and support teammates. A true asset to the team!",
+  name: "Jane Smith"
+},
+{
+  comment: "Delivers high-quality work consistently. Very reliable and dedicated!",
+  name: "Jim Brown"
 }
+];}
