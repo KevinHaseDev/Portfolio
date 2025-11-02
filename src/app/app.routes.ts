2e406ad3a57a4bb9a73909b694e948544c1e0a5b
Kevin Hase <hasekevin21@googlemail.com>
2025-11-02 18:58:33 +0100

    finish about me

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,18 @@
 import { Routes } from '@angular/router';
+import { Projects } from './components/projects/projects';
+import { Contact } from './components/contact/contact';
 import { MainPage } from './main-page/main-page';
+import { SkillSet } from './components/skill-set/skill-set';
+import { Comments } from './components/comments/comments';
+import { AboutMe } from './components/about-me/about-me';
+// import { LandingPage } from './components/landing-page/landing-page';
 
 export const routes: Routes = [
-    { path: '', component: MainPage }
+    { path: '', component: MainPage },
+    { path: 'projects', component: Projects },
+    { path: 'contact', component: Contact },
+    { path: 'skills', component: SkillSet },
+    { path: 'comments', component: Comments },
+    { path: 'about', component: AboutMe },
+    // { path: 'landing', component: LandingPage }
 ];
