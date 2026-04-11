7065874b973f16a067a54df765c21c8967043515
Kevin Hase <hasekevin21@googlemail.com>
2026-04-11 11:21:14 +0200

    combine css

--- src/app/components/projects/dialog/dialog.ts
+++ src/app/components/projects/dialog/dialog.ts
@@ -1,12 +1,11 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectDialog } from '../../../Interfaces/dialog.interface';
-import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-dialog',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './dialog.html',
   styleUrls: ['./dialog.scss']
 })
