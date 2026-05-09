b7fb6b05bae1b1d31a85391fa97c320b5dc5f6c0
Kevin Hase <hasekevin21@googlemail.com>
2026-05-09 12:17:46 +0200

    fix imprint

--- src/app/components/legal-notice/legal-notice.ts
+++ src/app/components/legal-notice/legal-notice.ts
@@ -6,18 +6,6 @@ type LegalNoticeContent = {
   imprintTitle: string;
   locationText: string;
   emailLabel: string;
-  acceptanceTitle: string;
-  acceptanceText: string;
-  scopeTitle: string;
-  scopeText: string;
-  rightsTitle: string;
-  rightsText: string;
-  useTitle: string;
-  useText: string;
-  disclaimerTitle: string;
-  disclaimerText: string;
-  indemnityTitle: string;
-  indemnityText: string;
   inquiryText: string;
   dateLabel: string;
 };
